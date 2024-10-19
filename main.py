ad29fba3a23e90d27fa6e56e6f2c6671f00b7257
Vladyslav Lysenko <vlad.lysenko1999@gmail.com>
2024-10-19 17:23:37 +0300

    Done Ex 1 from HM

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+def sum(a, b):
+    return a + b
+
 if __name__ == '__main__':
-    pass
+    print(sum(1, 2))
--- test.py
+++ test.py
@@ -2,4 +2,12 @@ import unittest
 
 
 class Test(unittest.TestCase):
-    pass
+    # Returns the correct sum of two positive integers
+    def test_sum_of_two_positive_integers(self):
+        result = sum(3, 5)
+        self.assertEqual(result, 8)
+
+    def test_sum_of_large_integers(self):
+        large_number = 10 ** 18
+        result = sum(large_number, large_number)
+        self.assertEqual(result, 2 * large_number)
